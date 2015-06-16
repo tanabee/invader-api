92d41ce528a5d4745736d48842d59d0f75d8b0f9
tanabee <tanabee1986@gmail.com>
2015-06-16 14:32:46 +0900

    トップランキング一覧取得 API 追加

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,7 +1,24 @@
-var Drama = require('../../models/user'),
+var User = require('../../models/user'),
     async = require('async');
 
 exports.getRanking = function(req, res) {
+  var conditions = {},
+      projection = {
+        _id: false
+      },
+      options    = {
+        sort: {
+          score: -1
+        }
+      };
+
+  User.find(conditions, projection, options, function (err, users) {
+    if (err) {
+      res.jsonp(500, err);
+      return;
+    }
+    res.json(users);
+  });
 };
 
 exports.getRankingMe = function(req, res) {
@@ -12,12 +29,16 @@ exports.getRankingMe = function(req, res) {
  * @param req.body.name ニックネーム
  */
 exports.register = function(req, res) {
+
   var name = req.body.name;
-  var user = new Drama({
+  var user = new User({
     name: name
   });
+
+  // DB 保存
   user.save(function (err, user) {
     if (err) {
+      // unique 制約に引っかかった場合
       if (err.code === 11000) {
         res.json(400, { message: 'This nickname already exists.' });
         return;
