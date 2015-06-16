3e1fba58f858e77fe8106d40d187428afbf18be6
tanabee <tanabee1986@gmail.com>
2015-06-16 17:20:25 +0900

    PUT to POST

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/v1/ranking'            , user.getRanking);
-app.get('/v1/ranking/me'         , user.getRankingMe);
-app.post('/v1/user'              , user.register);
-app.put('/v1/user/:userId/score' , user.updateScore);
+app.get('/v1/ranking'             , user.getRanking);
+app.get('/v1/ranking/me'          , user.getRankingMe);
+app.post('/v1/user'               , user.register);
+app.post('/v1/user/:userId/score' , user.updateScore);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
